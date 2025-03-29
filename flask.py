26dc254a11e1aff8b421c2869d43b5d48683e4bc
Anakin Landolfa <al5916@princeton.edu>
2025-03-29 17:00:12 -0400

    Flask server

---
+++ flask.py
@@ -0,0 +1,14 @@
+# Install flask
+pip install flask
+
+# Import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
