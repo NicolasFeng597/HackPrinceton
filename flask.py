2ecd95f87204ebecff158e561852f74e2647646c
Anakin Landolfa <al5916@princeton.edu>
2025-03-29 17:31:02 -0400

    Update flask.py

--- flask.py
+++ flask.py
@@ -2,13 +2,17 @@
 pip install flask
 
 # Import flask
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
 
+@app.route('/submit', methods=['GET'])
+def submit():
+    user_input = request.args.get('textInput')
+    
 if __name__ == '__main__':
     app.run(debug=True)
