02cd3fce31de418e0da07adcb05496094fcf786e
Anakin Landolfa <al5916@princeton.edu>
2025-03-29 18:03:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
 @app.route('/')
- def home():
-    return render_template('index.html')
- if __name__ == '__main__':
+def home():
+ return render_template('index.html')
+if __name__ == '__main__':
     app.run()
 
