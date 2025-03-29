0149d85fad1d81fa967ee3a561d13ebf861a3196
Anakin Landolfa <al5916@princeton.edu>
2025-03-29 19:40:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
